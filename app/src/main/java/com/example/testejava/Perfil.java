311fbf99d7aa90782d99a484ee58d19200644dae
Daniel Bezerra <daniel.bezerra@unicap.br>
2024-03-12 20:19:21 -0300

    activityResult

--- app/src/main/java/com/example/testejava/Perfil.java
+++ app/src/main/java/com/example/testejava/Perfil.java
@@ -4,6 +4,7 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.net.Uri;
@@ -14,11 +15,29 @@ import android.widget.ImageView;
 
 public class Perfil extends AppCompatActivity {
 
+    ActivityResultLauncher<String> mGetContent =
+            registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri uri){
+                    ImageView avatar = (ImageView) findViewById(R.id.avatar);
+                    avatar.setImageURI(uri);
+                }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
 
+        Button confirmar = (Button) findViewById(R.id.confirmar);
+
+        confirmar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGetContent.launch("image/*");
+            }
+        });
 
     }
 
